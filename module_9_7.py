6f5bc45f020dfc1574067a6e39a7defa7eb57b72
CDKNash <cdknash123@gmail.com>
2025-01-10 12:46:26 +0400

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+import math
+
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        n = func(*args, **kwargs)
+
+        if n % 2 != 0:
+            print("Простое")
+        else:
+             print("Составное")
+        return n
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    sum_ = a + b + c
+    return sum_
+
+result = sum_three(2, 3, 6)
+print(result)

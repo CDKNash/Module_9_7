f759834904ba1079c4770b996e341faf5a715608
CDKNash <cdknash123@gmail.com>
2025-01-18 17:13:18 +0400

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -3,12 +3,13 @@ import math
 def is_prime(func):
     def wrapper(*args, **kwargs):
         n = func(*args, **kwargs)
-
-        if n == 2 or n % 2 != 0:
-            print("Простое")
+        if n < 2:
+            return "Составное"
+        for i in range(2, int(n ** 0.5 + 1)):
+            if n % i == 0:
+                return "Составное"
         else:
-             print("Составное")
-        return n
+            return "Простое"
     return wrapper
 
 
